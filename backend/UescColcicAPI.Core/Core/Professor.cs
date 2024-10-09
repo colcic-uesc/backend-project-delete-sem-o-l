1408f745a578dc9e46ab49489f14f52ed55de6d0
Estêvão <sousaestevao17@gmail.com>
2024-10-09 19:29:29 -0300

    fix: adicionar novo atributo em Professor e atualizar ProfessorCRUD
    Alterações para o novo relacionamento com a tabela Project. Adicionado comentários no ProfessorCRUD para evitar bugs relacionados ao novo atributo.

--- backend/UescColcicAPI.Core/Core/Professor.cs
+++ backend/UescColcicAPI.Core/Core/Professor.cs
@@ -5,8 +5,9 @@ namespace UescColcicAPI.Core;
 public class Professor
 {
     public int ProfessorId { get; set; }
-    public string Name { get; set; } 
-    public string Email { get; set; }
-    public string Department { get; set; }
-    public string Bio { get; set; }
+    public required string Name { get; set; } 
+    public required string Email { get; set; }
+    public required string Department { get; set; }
+    public required string Bio { get; set; }
+    public required IEnumerable<int> Projects_FK { get; set; }
 }
