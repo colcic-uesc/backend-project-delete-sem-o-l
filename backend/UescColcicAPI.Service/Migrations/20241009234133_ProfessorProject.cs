87d7725b7a094b63b713f0ece875e2f9668fe009
Estêvão <sousaestevao17@gmail.com>
2024-10-09 22:39:51 -0300

    add: New Migrations

---
+++ backend/UescColcicAPI.Service/Migrations/20241009234133_ProfessorProject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UescColcicAPI.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfessorProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professors",
+                columns: table => new
+                {
+                    ProfessorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Department = table.Column<string>(type: "TEXT", nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", nullable: false),
+                    Projects_FK = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professors", x => x.ProfessorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false),
+                    type = table.Column<string>(type: "TEXT", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professors");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
---
+++ backend/UescColcicAPI.Service/Migrations/20241010013327_Projetonulo.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UescColcicAPI.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class Projetonulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Projects_FK",
+                table: "Professors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProfessorId",
+                table: "Projects",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProfessorId",
+                table: "Projects",
+                column: "ProfessorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Professors_ProfessorId",
+                table: "Projects",
+                column: "ProfessorId",
+                principalTable: "Professors",
+                principalColumn: "ProfessorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Professors_ProfessorId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProfessorId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProfessorId",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Projects_FK",
+                table: "Professors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
