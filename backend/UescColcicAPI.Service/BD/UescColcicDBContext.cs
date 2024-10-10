7810f35eb7d0d91311f626d2ced4585e2cb3b8e4
Estêvão <sousaestevao17@gmail.com>
2024-10-09 23:10:05 -0300

    fix: Alterar OnModelCreating

--- backend/UescColcicAPI.Service/BD/UescColcicDBContext.cs
+++ backend/UescColcicAPI.Service/BD/UescColcicDBContext.cs
@@ -9,17 +9,26 @@ public partial class MyDbContext : DbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<Skill> Skills { get; set; }
     public DbSet<Student_Skill> Student_Skills { get; set; }
+    public DbSet<Professor> Professors { get; set; }
+    public DbSet<Project> Projects {get; set;}
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Student_Skill>().HasKey(
             ss => new {ss.SkillId_FK, ss.StudentId_FK}
         );
-        base.OnModelCreating(modelBuilder);        
+
+        modelBuilder.Entity<Professor>()
+            .HasMany(p => p.Projects)
+            .WithOne()
+            .IsRequired(false);
+
+        base.OnModelCreating(modelBuilder);   
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=/home/rorizdavioliveira/UESC/colcic_web_2024_2/backend/UescColcicAPI/UescColcicAPI.db");
+        optionsBuilder.UseSqlite("Data Source=/home/estevao/Documentos/Estudos_Programação/Web/WEB-24.2/backend/UescColcicAPI/UescColcicAPI.db");
 
         base.OnConfiguring(optionsBuilder);
 
